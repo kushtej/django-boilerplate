2635986cbffb05cff1e8fe41cf829683e0bac1e7
kushtej <007kushtej@gmail.com>
2023-10-13 17:50:58 +0530

    Removed Exceptions JSON file

--- src/core/configurations/configurationhandler.py
+++ src/core/configurations/configurationhandler.py
@@ -12,12 +12,6 @@ RESOURCES_BASE_PATH = getattr(settings, "RESOURCES_BASE_PATH", BASE_APP_PATH + "
 # Path to web directory
 WEB_BASE_PATH = getattr(settings, "WEB_BASE_PATH", BASE_APP_PATH + "/web/")
 
-
-# Custom Exceptions in Custom Format
-EXCEPTIONS_FILE_PATH = BASE_APP_PATH + "/src/core/exceptions/exceptions.json"
-EXCEPTIONS = json.loads(open(EXCEPTIONS_FILE_PATH, encoding="utf-8").read())
-EXCEPTIONS = getattr(settings, "EXCEPTIONS", EXCEPTIONS)
-
 # Path to Configuration
 sys.path.insert(1, BASE_APP_PATH)
 from config import CONFIGURATION
